7677d039700604829d7cf81bafae2a0e23754df0
raymond.zhuang <zhuanglm@gmail.com>
2021-09-07 19:49:13 -0400

    Initial Commit

---
+++ upisdkdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  upisdkdemo
+//
+//  Created by Raymond Zhuang on 2021-09-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ upisdkdemo/upisdkdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  upisdkdemoApp.swift
+//  upisdkdemo
+//
+//  Created by Raymond Zhuang on 2021-09-07.
+//
+
+import SwiftUI
+
+@main
+struct upisdkdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
